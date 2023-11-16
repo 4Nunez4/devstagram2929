dfe78445458b183bf9c1d9e3006f8dbb20174cd5
diegokld30 <dfercalderon@poligran.edu.co>
2023-11-16 16:39:28 -0500

    Agregamos datos a la base de datos e interceptamos el username antes de la validacion por si viene con espacios los elimine y cambi por raya media

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller{
 
@@ -11,11 +14,22 @@ class RegisterController extends Controller{
         return view('auth.register');
     }
     public function store(Request $request){
+
+        $request->request->add(['username' => Str::slug($request->username)]);
+
         $this->validate($request, [
             'name'=>'required | max:30',
             'username'=>'required |unique:users|min:3|max:30',
             'email' =>'required |unique:users|email|max:60',
             'password' =>'required|confirmed|min:6'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+            //'password' => bcrypt($request->password)
         ]);
 
         dd("Insertando los datos del formulario");
